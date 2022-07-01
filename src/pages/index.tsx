5d49dba4fe998b92ae2bddbcbf649375f5816daf
Tumon <tumonkhn@gmail.com>
2022-07-01 16:02:26 +0600

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import Card from '../components/Card/Card'
+
+import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
 
@@ -12,14 +15,21 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     <div>
-     <Card color='Orange' smallMargin>
+     <div className={styles.container}>
+     {/* <Card color='Orange' smallMargin>
         This is Orange
       </Card>
 
       <Card color="Primary" smallMargin>
         This is Primary
-      </Card>
+      </Card> */}
+
+     
+     <Image src='/const.png' height={300} width={300} 
+      className={styles.img} alt='png' />
+      <h1> 
+        Under Construction
+      </h1>
      </div>
 
     </div>
